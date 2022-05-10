f73cad41442b3f6adfca9e7f8ec966a58647e3b4
ChickenSaysBak <nicholash2000@icloud.com>
2022-05-10 02:31:28 -0400

    Global access to the main instance

--- src/online/cozycloud/Islands/Islands.java
+++ src/online/cozycloud/Islands/Islands.java
@@ -4,10 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Islands extends JavaPlugin {
 
+    private Islands instance;
+    public Islands getInstance() {return instance;}
+
     @Override
     public void onEnable() {
 
-
+        instance = this;
 
     }
 
