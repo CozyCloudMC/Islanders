ab1584aa40eb409cbf13170c5c2c2277497fc67d
ChickenSaysBak <nicholash2000@icloud.com>
2022-05-11 02:20:06 -0400

    Schematic pasting

--- src/online/cozycloud/Islands/Islands.java
+++ src/online/cozycloud/Islands/Islands.java
@@ -1,10 +1,16 @@
 package online.cozycloud.Islands;
 
+import com.fastasyncworldedit.core.FaweAPI;
+import com.sk89q.worldedit.math.BlockVector3;
 import online.cozycloud.Islands.Commands.IslandsAdminCommand;
 import online.cozycloud.Islands.Commands.IslandsCommand;
 import online.cozycloud.Islands.Mechanics.TreeMechanics;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Islands extends JavaPlugin {
 
@@ -24,6 +30,18 @@ public class Islands extends JavaPlugin {
         getCommand("islandsadmin").setExecutor(new IslandsAdminCommand());
 
         Bukkit.getPluginManager().registerEvents(new TreeMechanics(), this);
+
+    }
+
+    public static boolean pasteSchematic(File file, Location loc, boolean pasteAir) {
+
+        try {
+            FaweAPI.load(file).paste(FaweAPI.getWorld(loc.getWorld().getName()), BlockVector3.at(loc.getX(), loc.getY(), loc.getZ()), false, pasteAir, null);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
 
     }
 
