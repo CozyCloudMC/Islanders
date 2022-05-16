227c88b3d3c494cf076da6405602beddca4b3545
ChickenSaysBak <nicholash2000@icloud.com>
2022-05-16 14:44:50 -0400

    Config refactor & local template world name

--- src/online/cozycloud/islands/Config.java
+++ src/online/cozycloud/islands/Config.java
@@ -4,37 +4,51 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 
 public class Config {
 
-    private File file;
+    private final File FILE;
 
     private String worldName;
+    private String localTemplateName;
 
     protected Config() {
-        file = new File(Islands.getInstance().getDataFolder(), "config.yml");
+        FILE = new File(Islands.getInstance().getDataFolder(), "config.yml");
         reload();
     }
 
-    //The following getters refer to config values and return defaults if no value exists.
+    public File getFile() {return FILE;}
 
+    //The following getters refer to config values and return defaults if no value exists.
     public String getWorldName() {return worldName != null ? worldName : "islands";}
+    public String getLocalTemplateName() {return localTemplateName != null ? localTemplateName : "local_template";}
 
     public void reload() {
 
-        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
-        if (!file.exists()) initialize(config);
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(FILE);
+        if (!FILE.exists()) initialize(config);
 
-        worldName = config.getKeys(false).contains("world_name") ? config.getString("world_name") : getWorldName();
+        Set<String> rootSection = config.getKeys(false);
+
+        if (rootSection.contains("worlds")) {
+
+            Set<String> worldsSection = config.getConfigurationSection("worlds").getKeys(false);
+
+            if (worldsSection.contains("world_name")) worldName = config.getString("worlds.world_name");
+            if (worldsSection.contains("local_template_name")) localTemplateName = config.getString("worlds.local_template_name");
+
+        }
 
     }
 
     private void initialize(YamlConfiguration config) {
 
-        config.set("world_name", getWorldName());
+        config.set("worlds.world_name", getWorldName());
+        config.set("worlds.local_template_name", getLocalTemplateName());
 
         try {
-            config.save(file);
+            config.save(FILE);
         } catch (IOException e) {
             e.printStackTrace();
         }
