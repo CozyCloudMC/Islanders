72be47a0c787e991e30d79fdc8f65bb34ba5fd84
ChickenSaysBak <nicholash2000@icloud.com>
2022-05-17 23:10:54 -0400

    Added SQL support and island creation cancellation

--- src/online/cozycloud/islands/local/LocalIslandManager.java
+++ src/online/cozycloud/islands/local/LocalIslandManager.java
@@ -11,6 +11,7 @@ import org.jetbrains.annotations.Nullable;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -47,38 +48,47 @@ public class LocalIslandManager {
 
     /**
      * Creates a local island for the specified player(s).
-     * File duplication is run asynchronously to avoid lagging the main thread.
-     * World loading through Bukkit must be synchronous.
      * @param players the players starting the island
      */
     public void createIsland(List<Player> players) {
 
         for (Player p : players) p.sendMessage(ChatColor.GRAY + "Creating your island...");
 
-        // Async
+        // SQL operations and file duplication are run asynchronously to avoid lagging the main thread
         Bukkit.getScheduler().runTaskAsynchronously(Islands.getInstance(), () -> {
 
             String name = createWorldName();
+            ArrayList<UUID> members = new ArrayList<>();
+            for (Player p : players) members.add(p.getUniqueId());
+
+            try {
+                createData(name, members);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                cancelIslandCreation(name, players);
+                return;
+            }
 
             try {
                 duplicateTemplate(name);
             } catch (IOException e) {
                 e.printStackTrace();
-                for (Player p : players) p.sendMessage(ChatColor.RED + "An error occurred. Contact an admin for help!");
+                cancelIslandCreation(name, players);
                 return;
             }
 
-            // Sync
+            // World loading must be synchronous
+            // Must occur *after* the folder exists, or it will make its own empty world
             Bukkit.getScheduler().runTask(Islands.getInstance(), () -> {
 
                 World world = setUpWorld(name);
 
                 if (world == null) {
-                    for (Player p : players) p.sendMessage(ChatColor.RED + "An error occurred. Contact an admin for help!");
+                    cancelIslandCreation(name, players);
                     return;
                 }
 
-                LocalIsland island = new LocalIsland(name, players);
+                LocalIsland island = new LocalIsland(name, members);
                 localIslands.put(name, island);
 
                 for (Player p : players) {
@@ -87,6 +97,34 @@ public class LocalIslandManager {
                 }
 
             });
+
+        });
+
+    }
+
+    /**
+     * Deletes database data, deletes the world folder of the specified island, and sends the specified players an error message.
+     * This is to be used when part of the island creation fails; this prevents incomplete island setups.
+     * @param name the island name
+     * @param players the players attempting to start an island
+     */
+    private void cancelIslandCreation(String name, List<Player> players) {
+
+        for (Player p : players) p.sendMessage(ChatColor.RED + "An error occurred. Contact an admin for help!");
+
+        Bukkit.getScheduler().runTaskAsynchronously(Islands.getInstance(), () -> {
+
+            // Removes SQL data if it exists
+            try {
+                String deleteCmd = "DELETE FROM local_islands WHERE name = '" + name + "';";
+                Islands.getSqlHandler().getConnection().prepareStatement(deleteCmd).executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            // Deletes world folder if it exists
+            File file = new File(Islands.getWorldHandler().getWorldFolder(), name);
+            if (file.exists() && !file.delete()) Islands.getInstance().getLogger().warning("Failed to delete world folder: " + name);
 
         });
 
@@ -104,7 +142,7 @@ public class LocalIslandManager {
 
     /**
      * Duplicates local template world folder without the uid.dat file.
-     * @param name the world name
+     * @param name the island name
      * @throws IOException thrown if the file could not be duplicated
      */
     private void duplicateTemplate(String name) throws IOException {
@@ -118,8 +156,19 @@ public class LocalIslandManager {
     }
 
     /**
+     * Inserts local island data into the database.
+     * @param name the island name
+     * @param members the UUIDs of the island's members
+     * @throws SQLException thrown if a connection could not be made to the database
+     */
+    private void createData(String name, List<UUID> members) throws SQLException {
+        String insertCmd = "INSERT INTO local_islands(name, members) VALUES ('" + name + "', '" + membersToString(members) + "');";
+        Islands.getSqlHandler().getConnection().prepareStatement(insertCmd).executeUpdate();
+    }
+
+    /**
      * Loads the specified local world and establishes basic properties like game rules.
-     * @param name the world name
+     * @param name the island name
      * @return the newly loaded world or null if setup failed
      */
     @Nullable
@@ -139,6 +188,35 @@ public class LocalIslandManager {
         world.setGameRule(GameRule.MOB_GRIEFING, false);
 
         return world;
+
+    }
+
+    /**
+     * Convert a list of UUIDs to a string that can be saved to the database.
+     * @param members a list of the island members' UUIDs
+     * @return a string of UUIDs separated by commas
+     */
+    public static String membersToString(List<UUID> members) {
+
+        String result = "";
+        for (UUID uuid : members) result += uuid.toString() + ",";
+        if (result.endsWith(",")) result = result.substring(0, result.length()-1);
+
+        return result;
+
+    }
+
+    /**
+     * Convert a string to a list of UUIDs.
+     * @param members a comma separated string of UUIDs
+     * @return a list of UUIDs
+     */
+    public static ArrayList<UUID> membersToList(String members) {
+
+        ArrayList<UUID> result = new ArrayList<>();
+        for (String member : members.split(",")) result.add(UUID.fromString(member));
+
+        return result;
 
     }
 
