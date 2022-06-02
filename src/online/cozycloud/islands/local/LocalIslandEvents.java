3e9faa46f432ff31692e0368d9f187b12be5320c
ChickenSaysBak <nicholash2000@icloud.com>
2022-06-02 18:49:48 -0400

    Custom nether portal functionality

--- src/online/cozycloud/islands/local/LocalIslandEvents.java
+++ src/online/cozycloud/islands/local/LocalIslandEvents.java
@@ -3,6 +3,7 @@ package online.cozycloud.islands.local;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import online.cozycloud.islands.Islands;
+import online.cozycloud.islands.mechanics.worlds.WorldHandler;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -10,15 +11,24 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityPortalEnterEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.event.world.PortalCreateEvent;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class LocalIslandEvents implements Listener {
 
+    private HashMap<UUID, Long> lastNetherPortalCheck = new HashMap<>();
+    private HashMap<Player, BukkitTask> teleporting = new HashMap<>();
+
     public LocalIslandEvents() {
         runBoundsCheck();
+        runAbortTPCheck();
     }
 
     /**
@@ -44,6 +54,26 @@ public class LocalIslandEvents implements Listener {
             }
 
         }, 1, 7);
+
+    }
+
+    /**
+     * Checks if a player is no longer in a nether portal and removes them from the teleporting list.
+     */
+    private void runAbortTPCheck() {
+
+        Bukkit.getScheduler().runTaskTimer(Islands.getInstance(), () -> {
+
+            for (Player p : new ArrayList<>(teleporting.keySet())) {
+
+                if (p == null || !p.isOnline() || !isInNetherPortal(p)) {
+                    teleporting.get(p).cancel();
+                    teleporting.remove(p);
+                }
+
+            }
+
+        }, 1, 1);
 
     }
 
@@ -95,6 +125,7 @@ public class LocalIslandEvents implements Listener {
 
         Player player = e.getPlayer();
         Block block = e.getBlock();
+        
 
         if (block.getBiome() == LocalIslandManager.getOutterBiome(e.getPlayer().getWorld().getEnvironment()) && player.getGameMode() == GameMode.SURVIVAL) {
             e.setCancelled(true);
@@ -103,7 +134,7 @@ public class LocalIslandEvents implements Listener {
 
     }
     @EventHandler
-    public void onPlayerPlace(BlockPlaceEvent e){
+    public void onPlayerPlace(BlockPlaceEvent e) {
 
         Player player = e.getPlayer();
         Block block = e.getBlock();
@@ -128,6 +159,85 @@ public class LocalIslandEvents implements Listener {
 
         // Ran 1 tick later so that the player is not still in the world
         Bukkit.getScheduler().runTaskLater(Islands.getInstance(), () -> Islands.getLocalIslandManager().unloadInactiveIslandWorlds(), 1);
+
+    }
+
+    @EventHandler // Disables default nether portal creation because it is created in the wrong world
+    public void onPortalCreate(PortalCreateEvent e) {
+        if (e.getReason() == PortalCreateEvent.CreateReason.NETHER_PAIR) e.setCancelled(true);
+    }
+
+    @EventHandler // Disables default nether portal player teleportation
+    public void onPlayerPortalTP(PlayerPortalEvent event) {
+        if (event.getCause() == PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) event.setCancelled(true);
+    }
+
+    @EventHandler // Called constantly when entity is in a portal
+    public void onPortalEnter(EntityPortalEnterEvent e) {
+
+        if (e.getEntity() instanceof Player player && e.getLocation().getBlock().getType() == Material.NETHER_PORTAL) {
+            lastNetherPortalCheck.put(player.getUniqueId(), System.currentTimeMillis());
+            initiateNetherTeleport(player);
+        }
+
+    }
+
+    /**
+     * Prepares to teleport a player standing in a nether portal.
+     * @param player the player to teleport
+     */
+    private void initiateNetherTeleport(Player player) {
+
+        UUID uuid = player.getUniqueId();
+        if (teleporting.containsKey(player)) return;
+
+        World world = player.getWorld();
+
+        // Creates a local island's nether if it does not exist
+        if (world.getEnvironment() == World.Environment.NORMAL) {
+            LocalIsland island = Islands.getLocalIslandManager().getIsland(world);
+            if (island != null && !island.hasWorld(World.Environment.NETHER)) LocalIslandManager.getLocalIslandSetupManager().addWorld(island.getID(), World.Environment.NETHER);
+        }
+
+        teleporting.put(player, Bukkit.getScheduler().runTaskLater(Islands.getInstance(), () -> {
+
+            if (player.isOnline() && isInNetherPortal(player)) {
+                netherTeleport(player);
+                teleporting.remove(player);
+            }
+
+        }, 80));
+
+    }
+
+    /**
+     * Teleports a player through a nether portal to the corresponding world.
+     * @param player the player to teleport
+     */
+    private void netherTeleport(Player player) {
+
+        World world = player.getWorld();
+        World.Environment toEnvironment = world.getEnvironment() == World.Environment.NORMAL ? World.Environment.NETHER : World.Environment.NORMAL;
+        LocalIsland island = Islands.getLocalIslandManager().getIsland(world);
+
+        // Does not attempt to load world in case the world creation is currently taking place
+        World toWorld = island != null ? island.getWorld(toEnvironment, false) : WorldHandler.getRelatedDimension(world, toEnvironment);
+        if (toWorld != null) player.teleport(toWorld.getSpawnLocation());
+
+    }
+
+    /**
+     * Checks if a player is in a nether portal.
+     * @param player the player to check
+     * @return true if in nether portal
+     */
+    private boolean isInNetherPortal(Player player) {
+
+        UUID uuid = player.getUniqueId();
+        if (!lastNetherPortalCheck.containsKey(uuid)) return false;
+
+        // Last check must have happened at least 100 milliseconds ago
+        return System.currentTimeMillis() - lastNetherPortalCheck.get(uuid) <= 100;
 
     }
 
