1c9d214daeb0822223ee53c0499f83984d4f8bca
ChickenSaysBak <nicholash2000@icloud.com>
2022-06-08 00:13:46 -0400

    Replaced "test" sub-command with "delete"

--- src/online/cozycloud/islands/commands/IslandsAdminCommand.java
+++ src/online/cozycloud/islands/commands/IslandsAdminCommand.java
@@ -65,20 +65,10 @@ public class IslandsAdminCommand implements TabExecutor {
 
                     }
 
-                    else if (args[0].equalsIgnoreCase("test") && sender instanceof Player player) {
+                    else if (args[0].equalsIgnoreCase("delete") && sender instanceof Player player) {
 
-                        if (args.length >= 2) {
-
-                            if (args[1].equalsIgnoreCase("create")) LocalIslandManager.getLocalIslandSetupManager().createIsland(List.of(player));
-
-                            else if (args[1].equalsIgnoreCase("delete")) {
-
-                                if (args.length >= 3) LocalIslandManager.getLocalIslandSetupManager().deleteIsland(args[2], sender);
-                                else sender.sendMessage(ChatColor.RED + "Usage: /" + label + " test delete <name>");
-
-                            } else sender.sendMessage(ChatColor.RED + "Usage: /" + label + " test <create|delete>");
-
-                        } else sender.sendMessage(ChatColor.RED + "Usage: /" + label + " test <create|delete>");
+                        if (args.length >= 2) LocalIslandManager.getLocalIslandSetupManager().deleteIsland(args[1], sender);
+                        else sender.sendMessage(ChatColor.RED + "Usage: /" + label + " delete <name>");
 
                     } else sender.sendMessage(usage);
 
@@ -101,24 +91,19 @@ public class IslandsAdminCommand implements TabExecutor {
 
             switch (args.length) {
 
-                case 1 -> result.addAll(List.of("reload", "worlds", "tp", "test"));
+                case 1 -> result.addAll(List.of("reload", "worlds", "tp", "delete"));
 
                 case 2 -> {
 
                     if (args[0].equalsIgnoreCase("tp")) for (World w : Bukkit.getWorlds()) {
                         String name = w.getName();
                         if (name.toLowerCase().startsWith(args[1].toLowerCase())) result.add(name); // Hides irrelevant results
-                    } else if (args[0].equalsIgnoreCase("test")) result.addAll(List.of("create", "delete"));
+                    }
 
-                }
-
-                case 3 -> {
-
-                    if (args[0].equalsIgnoreCase("test") && args[1].equalsIgnoreCase("delete"))
-                        for (LocalIsland island : Islands.getLocalIslandManager().getIslands()) {
-                            String name = island.getID();
-                            if (name.toLowerCase().startsWith(args[2].toLowerCase())) result.add(name);
-                        }
+                    else if (args[0].equalsIgnoreCase("delete")) for (LocalIsland island : Islands.getLocalIslandManager().getIslands()) {
+                        String name = island.getID();
+                        if (name.toLowerCase().startsWith(args[1].toLowerCase())) result.add(name);
+                    }
 
                 }
 
